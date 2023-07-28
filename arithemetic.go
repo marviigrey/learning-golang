a9921794296deacaa9c5c90526a15c20b71c2031
marviigrey <marvellousfrederick@gmail.com>
2023-07-28 18:53:36 +0000

    arithemetic operators

---
+++ arithemetic.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	var a, b int = 12, 32
+	
+	var c, d float64 = 32.33, 20.60
+	
+	var g, h int = 9, 4
+	
+	var e, f string = "GOD is ", "Good"
+	
+	var y int = 23
+
+	y++
+
+	var z int = 45
+	z--
+
+	fmt.Println(a + b)
+	
+	fmt.Println(c - d)
+	
+	fmt.Println(e + f)
+	
+	fmt.Println(g * h)
+	
+	fmt.Println(b / a)
+	
+	fmt.Println(b % a)
+	
+	fmt.Println(y)
+	
+	fmt.Println(z)
+
+
+}
